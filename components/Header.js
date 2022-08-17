46845984f00d1c023a547bfd85f2790647863c43
AayushMohan <aayushmohan1702@gmail.com>
2022-08-18 01:07:23 +0530

    Added: Styles

--- components/Header.js
+++ components/Header.js
@@ -19,13 +19,13 @@ const Header = () => {
 export default Header;
 
 const Wrapper = styled.div`
-  width: calc(100% - 3rem);
+  width: calc(100%);
+  /* TRouBLe */
   padding: 1rem 1.5rem;
   border-bottom: 1px solid #282b2f;
   display: flex;
   align-items: center;
 `;
-
 const Title = styled.div`
   font-size: 2rem;
   font-weight: 600;
@@ -36,17 +36,42 @@ const ButtonsContainer = styled.div`
   display: flex;
 `;
 
+const WalletLink = styled.div`
+  font-size: 0.8rem;
+  border: 1px solid #282b2f;
+  border-radius: 50rem;
+  font-size: 1.2rem;
+  margin-right: 1rem;
+  padding: 0 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+`;
+
+const WalletLinkTitle = styled.div`
+  font-size: 1.1rem;
+  margin-bottom: 0.3rem;
+  color: #27ad75;
+  font-weight: 600;
+`;
+const WalletAddress = styled.div`
+  font-size: 0.8rem;
+  /* color: #8a919e; */
+`;
+
 const Button = styled.div`
   border: 1px solid #282b2f;
   padding: 0.8rem;
   font-size: 1.3rem;
   font-weight: 500;
   border-radius: 0.4rem;
-  background-color: #3775f5;
-  color: #000;
   margin-right: 1rem;
-
   &:hover {
     cursor: pointer;
   }
 `;
+
+const Separator = styled.div``;
+
+const ProfileIcon = styled.div``;
--- pages/index.js
+++ pages/index.js
@@ -28,26 +28,28 @@ const Wrapper = styled.div`
   display: flex;
   height: 100vh;
   max-width: 100vw;
-  background-color: #0a0b0b;
+  background-color: #0a0b0d;
   color: white;
   display: grid;
   place-items: center;
 `;
+
 const WalletConnect = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
+
 const Button = styled.div`
+  /* flex: 0; */
   border: 1px solid #282b2f;
   padding: 0.8rem;
-  font-size: 1.2rem;
+  font-size: 1.3rem;
   font-weight: 500;
   border-radius: 0.4rem;
-  background-color: #3775f5;
+  background-color: #3773f5;
   color: #000;
-
   &:hover {
     cursor: pointer;
   }
